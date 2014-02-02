76631a82b53c37c9a549569223211bf088b4a5aa
Saif Hakim <saif7463@gmail.com>
2014-02-02 03:50:20 -0800

    compile watch for scss/coffee. basic template made.

--- manage.py
+++ manage.py
@@ -1,7 +1,15 @@
+import subprocess
 from flask.ext.script import Manager
 from moo import app
 
 manager = Manager(app)
+
+@manager.command
+def compile(watch=True):
+  flags = '-wco' if watch else '-co'
+  coffee = subprocess.Popen('coffee %s moo/static/js moo/coffee' % flags, shell=True)
+  scss = subprocess.Popen('scss -w moo/scss:moo/static/css', shell=True)
+  #coffee.wait()
 
 if __name__ == "__main__":
     manager.run()
--- moo/__init__.py
+++ moo/__init__.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 
 @app.route("/")
 def hello():
-  return "Hello World!"
+  return render_template('moo.jade')
 
 if __name__ == "__main__":
   app.run()
